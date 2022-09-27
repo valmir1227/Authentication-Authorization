cb292ac5271679aa7ea72fc961fb43cc584f1dd7
Valmir Almeida <almeidavalmir76@gmail.com>
2022-09-26 21:28:40 -0300

    :package: Saving tokens in cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,14 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
+import { setCookie } from "nookies";
+
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SingInCredentials = {
   email: string;
@@ -7,6 +17,7 @@ type SingInCredentials = {
 
 type AuthContextData = {
   singIn(Credentials: SingInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 };
 
@@ -17,13 +28,39 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = !!user;
 
   async function singIn({ email, password }: SingInCredentials) {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextAuth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30days
+        path: "/",
+      });
+      setCookie(undefined, "nextAuth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30days
+        path: "/",
+      });
+
+      setUser({ email, permissions, roles });
+
+      Router.push("/dashboard");
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
-    <AuthContext.Provider value={{ singIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ singIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
