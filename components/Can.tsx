85a5f6949906d9896f1b6e601cfb78c7692f2f3b
Valmir Almeida <almeidavalmir76@gmail.com>
2022-10-01 22:58:53 -0300

    :packae: Creating permission component

--- components/Can.tsx
+++ components/Can.tsx
@@ -1,6 +1,18 @@
+import { ReactNode } from "react";
+import { useCan } from "../hooks/useCan";
 
-export default function Can() {
-  return (
-    <div>Can</div>
-  )
+interface CanProps {
+  children: ReactNode;
+  permissions?: string[];
+  roles?: string[];
+}
+
+export default function Can({ children, permissions, roles }: CanProps) {
+  const useCanSeeComponent = useCan({ permissions, roles });
+
+  if (!useCanSeeComponent) {
+    return null;
+  }
+
+  return <>{children}</>;
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,10 +5,10 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { useCan } from "../hooks/useCan";
+import Can from "../components/Can";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-  const userCanSeeMetrics = useCan({ permissions: ["metrics.list"] });
 
   useEffect(() => {
     api.get("/me").then((response) => console.log(response));
@@ -17,7 +17,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={["metrics.list"]}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
